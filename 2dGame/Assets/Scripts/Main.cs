4fea2d0ac817a55aef388e21f244aa953c440090
MichaelSki82 <romantik1001@yandex.ru>
2021-12-13 18:45:11 +0300

    HW1
    Сделал движение анимации

---
+++ 2dGame/Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skipin2D
+{
+
+    public class Main : MonoBehaviour
+    {
+        [SerializeField] private SpriteAnimatorConfig _playerConfig;
+        [SerializeField] private int _animationSpeed = 15;
+        [SerializeField] private LevelObjectView _playerView;
+
+        private SpriteAnimatorController _playerAnimator;
+
+        void Awake()
+        {
+            _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
+
+            _playerAnimator = new SpriteAnimatorController(_playerConfig);
+
+            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+        }
+
+        void Update()
+        {
+            _playerAnimator.Update();
+        }
+    }
+}
