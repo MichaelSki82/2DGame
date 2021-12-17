467aed186717a17a5614de0d159a9b8373aa18de
MichaelSki82 <romantik1001@yandex.ru>
2021-12-17 11:02:54 +0300

    HW2
    движение персонажа, наброски платформ, и пушки

--- 2dGame/Assets/Scripts/Main.cs
+++ 2dGame/Assets/Scripts/Main.cs
@@ -8,10 +8,13 @@ namespace Skipin2D
     public class Main : MonoBehaviour
     {
         [SerializeField] private SpriteAnimatorConfig _playerConfig;
-        [SerializeField] private int _animationSpeed = 15;
+        //[SerializeField] private int _animationSpeed = 15;
         [SerializeField] private LevelObjectView _playerView;
 
+
+
         private SpriteAnimatorController _playerAnimator;
+        private PlayerController _playerController;
 
         void Awake()
         {
@@ -19,12 +22,15 @@ namespace Skipin2D
 
             _playerAnimator = new SpriteAnimatorController(_playerConfig);
 
-            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+            //_playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+
+            _playerController = new PlayerController(_playerView, _playerAnimator);
         }
 
         void Update()
         {
-            _playerAnimator.Update();
+            //_playerAnimator.Update();
+            _playerController.Update();
         }
     }
 }
